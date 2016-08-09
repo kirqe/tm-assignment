8f4407a4d3a33317bdb6b222e36a06431e4463d9
railsr <ikll7@yahoo.com>
2016-08-09 05:12:18 +0400

    ...

--- spec/controllers/web/tasks_controller_spec.rb
+++ spec/controllers/web/tasks_controller_spec.rb
@@ -2,9 +2,19 @@ require 'rails_helper'
 
 RSpec.describe Web::TasksController, :type => :controller do
   describe "GET index" do
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
+    context "user authenticated" do
+
+      # it "renders the index template" do
+      #   get :index
+      #   expect(response).to render_template("index")
+      # end
     end
+    context "user not authenticated" do
+      it "redirects user to login_path" do
+        get :index
+        expect(response).to redirect_to login_path
+      end
+    end
+
   end
 end
