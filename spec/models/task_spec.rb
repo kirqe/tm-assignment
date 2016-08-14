525c6d6fd3310c4853a8e58599d8d60b9b915ca3
railsr <ikll7@yahoo.com>
2016-08-14 04:38:32 +0400

    aasm tests

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,4 +9,24 @@ RSpec.describe Task, :type => :model do
     user = FactoryGirl.create(:user, role: 'user')
     FactoryGirl.create(:task, user: user).should be_valid
   end
+
+  describe "task state" do
+    user = FactoryGirl.create(:user, role: 'user')
+    task = FactoryGirl.create(:task, user: user)
+
+    it "has state 'new' when just created" do
+      expect(task.state).to eq("new")
+    end
+
+    it "has state 'started' after starting" do
+      task.start!
+      expect(task.state).to eq("started")
+    end
+
+    it "has state 'finished' after finishing" do
+      task.finish!
+      expect(task.state).to eq("finished")
+    end
+  end
+
 end
